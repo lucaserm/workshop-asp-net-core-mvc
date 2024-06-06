f1b798eb863d362b15cf79ffc82d11a96587bfa9
Lucas Macedo <lucasedumacedo@gmail.com>
2024-06-06 08:32:58 -0400

    Updating our sources

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Salles Web MVC App from C# Course";
-            ViewData["Professor"] = "Nelio Alves";
+            ViewData["Professor"] = "Lucas Macedo";
 
             return View();
         }
